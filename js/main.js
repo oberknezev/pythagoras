324a243856e47de170026b12b8269f7c9fc8c897
oberknezev <oberknezev@yahoo.com>
2018-01-01 20:55:34 +0100

    adding simple validation

--- js/main.js
+++ js/main.js
@@ -2,14 +2,20 @@ function checkPyth(){
     // alert("sve ok"); ovde proveravam da li je fcija povezana ok
     // debugger;
     var inputA = document.getElementById("aSide").value; //zasto je ovde bitno staviti ' a ne "   ???
-    alert(inputA);//provera da li je dobro uhvacen
+    // alert(inputA);//provera da li je dobro uhvacen
+    if( isNaN(inputA)){
+           document.getElementById("aMessage").innerHTML = "unesi broj bre"; //ovo prepravi za kasnije
+    }
     var inputB = document.getElementById("bSide").value;
     var inputC = document.getElementById("cSide").value;
-    alert(inputA + ", " + inputB + ", " + inputC); //da bih predstavio tri varijable, svaku odvojenu zarezima
-
-    if (inputA*inputA + inputB*inputB == inputC*inputC || inputB*inputB + inputC*inputC == inputA*inputA || inputA*inputA + inputC*inputC == inputB*inputB) {
-        alert("jeste");
+    // alert(inputA + ", " + inputB + ", " + inputC); //da bih predstavio tri varijable, svaku odvojenu zarezima
+    var result = document.getElementById("result");
+    debugger;
+    if (inputA*inputA + inputB*inputB === inputC*inputC || inputB*inputB + inputC*inputC === inputA*inputA || inputA*inputA + inputC*inputC === inputB*inputB) {
+        result.innerHTML = "jeste pravougli";
     } else {
-            alert("nije")
-        }
+        result.innerHTML = "nije pravougli";
+    }
+
+
 }
