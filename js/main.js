5bccdf255e921f3ee226abaea1f9285de5767663
oberknezev <oberknezev@yahoo.com>
2018-01-06 00:51:47 +0100

    small changes and translations

--- js/main.js
+++ js/main.js
@@ -1,20 +1,18 @@
 function checkPyth(){
-    // alert("sve ok"); ovde proveravam da li je fcija povezana ok
     // debugger;
-    var inputA = document.getElementById("aSide").value; //zasto je ovde bitno staviti ' a ne "   ???
-    // alert(inputA);//provera da li je dobro uhvacen
+    var inputA = document.getElementById("aSide").value;
+
     if( isNaN(inputA)){
-           document.getElementById("aMessage").innerHTML = "unesi broj bre"; //ovo prepravi za kasnije
+           document.getElementById("aMessage").innerHTML = "Please enter a numeric value";
     }
     var inputB = document.getElementById("bSide").value;
     var inputC = document.getElementById("cSide").value;
-    // alert(inputA + ", " + inputB + ", " + inputC); //da bih predstavio tri varijable, svaku odvojenu zarezima
     var result = document.getElementById("result");
     debugger;
     if (inputA*inputA + inputB*inputB === inputC*inputC || inputB*inputB + inputC*inputC === inputA*inputA || inputA*inputA + inputC*inputC === inputB*inputB) {
-        result.innerHTML = "jeste pravougli";
+        result.innerHTML = "Triangle with this sides is a right trinagle";
     } else {
-        result.innerHTML = "nije pravougli";
+        result.innerHTML = "Triangle with this sides is NOT a right trinagle";
     }
 
 
